0a5b029fe493adcfb1bbf3c72ee22c5f37757d5e
prasannakrishna.b <prasannakrishna2011@gmail.com>
2024-12-06 18:21:35 +0530

    service added

---
+++ src/main/java/com/bhagwat/scm/productService/dto/Address.java
@@ -0,0 +1,27 @@
+package com.bhagwat.scm.productService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    private Long id;
+    private Long partyId; // Party reference from Party table
+    private String line1;
+    private String line2;
+    private String line3;
+    private String post;
+    private String city;
+    private String district;
+    private String state;
+    private String country;
+    private String pincode;
+    private String contactNumber;
+    private String email;
+
+    // Constructor, Getters and Setters
+}
+
